55b818935c2df3c70f67e0b9d61d3ff739886924
oceane92 <34437842+oceane92@users.noreply.github.com>
2018-04-26 15:58:23 +0200

    Database 1

--- app/src/main/java/com/delafond/oceane/poker/repository/DatabaseManager.java
+++ app/src/main/java/com/delafond/oceane/poker/repository/DatabaseManager.java
@@ -4,50 +4,35 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Base de données pour les scores
- */
 public class DatabaseManager extends SQLiteOpenHelper {
 
-    private static final String DATABASE_NAME = "PokerBase";
-    private static final int CURRENT_DB_VERSION = 1;
+    private static final String TABLE_SCORE = "table_score";
+    private static final String COL_ID = "ID";
+    private static final String COL_PLAYER = "Player";
+    private static final String COL_OPPONENT = "Opponent";
+    private static final String COL_WINLOSE = "WinOrLose";
+    private static final String COL_MAIN = "Main";
 
-    private static DatabaseManager instance;
+    private static final String CREATE_BDD = "CREATE TABLE " + TABLE_SCORE + " ("
+            + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_PLAYER + " TEXT NOT NULL, "
+            + COL_OPPONENT + " TEXT NOT NULL, " + COL_WINLOSE + " TEXT NOT NULL, " + COL_MAIN + " TEXT NOT NULL);";
 
-    public static DatabaseManager getInstance(Context context){
-        if(instance==null){
-            instance = new DatabaseManager(context);
-        }
-        return instance;
-    }
-
-    private DatabaseManager(Context context) {
-        super(context, DATABASE_NAME, null, CURRENT_DB_VERSION);
+    public DatabaseManager(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
     }
 
     /**
      * Création de la base de données
      */
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table score "+
-                "(id INTEGER, player TEXT, adversary TEXT, winlose TEXT, "+
-                "main FLOAT);");
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BDD);
     }
 
-    /**
-     * Mise à jour de la base de données si changement de version
-     */
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
-        switch (i){
-            case 1:
-                //code sql 1->2
-            case 2:
-                //code sql 2->3
-            case 3:
-                //code sql 3->4
-            default:
-        }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE " + TABLE_SCORE + ";");
+        onCreate(db);
     }
+
 }
